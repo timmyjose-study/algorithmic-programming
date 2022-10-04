dbf5fd4b36643fa169349ec22d38dee0829706a3
Timmy Jose <zoltan.jose@gmail.com>
2022-10-04 10:18:51 +0530

    Grind - Patterns - Fast and Slow Pointers

---
+++ prep/implementation/fast_and_slow_pointers/StartOfLinkedListCycle.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+
+// O(n) / O(1)
+public class StartOfLinkedListCycle {
+  static class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
+
+  public static void main(String[] args) {
+    try (Scanner in = new Scanner(System.in)) {
+      int tt = in.nextInt();
+
+      while (tt-- > 0) {
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        ListNode head = new ListNode(1);
+        ListNode curr = head;
+        for (int i = 2; i <= n; i++) {
+          curr.next = new ListNode(i);
+          curr = curr.next;
+        }
+
+        if (k != -1) {
+          ListNode tmp = head;
+          for (int i = 0; i < k - 1; i++) {
+            tmp = tmp.next;
+          }
+          curr.next = tmp;
+        }
+
+        System.out.println(startOfCycle(head));
+      }
+    }
+  }
+
+  private static int startOfCycle(ListNode head) {
+    ListNode fast = head, slow = head;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+
+      if (fast == slow) {
+        ListNode tmp = head;
+        while (tmp != slow) {
+          tmp = tmp.next;
+          slow = slow.next;
+        }
+
+        return slow.val;
+      }
+    }
+
+    return -1;
+  }
+}
