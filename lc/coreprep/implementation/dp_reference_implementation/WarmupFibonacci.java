eac7ee7327fbc153c35d4c70cbb9156bdefefb29
Timmy Jose <zoltan.jose@gmail.com>
2022-11-11 08:48:14 +0530

    Grind - DP - Starting out (warmup)

---
+++ lc/coreprep/implementation/dp_reference_implementation/WarmupFibonacci.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+
+public class WarmupFibonacci {
+  public static void main(String[] args) {
+    try (Scanner in = new Scanner(System.in)) {
+      int tt = in.nextInt();
+
+      while (tt-- > 0) {
+        int n = in.nextInt();
+        System.out.println(fibonacci(n));
+      }
+    }
+  }
+
+  // O(n) / O(n)
+  public static long fibonacci(int n) {
+    long[] dp = new long[n + 1];
+
+    if (n == 0) {
+      return 0;
+    }
+
+    if (n == 1) {
+      return 1;
+    }
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+  }
+}
---
+++ lc/coreprep/implementation/dp_reference_implementation/warmup_fibonacci.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <queue>
+#include <random>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int next_random(int low, int high) {
+  random_device rd;
+  mt19937 engine(rd());
+  uniform_int_distribution<int> dist(low, high);
+
+  return dist(engine);
+}
+
+// O(n) / O(n)
+long fibo(int n) {
+  if (n == 0 || n == 1) {
+    return n;
+  }
+
+  vector<long> dp(n + 1);
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for (int i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int tt, n;
+  cin >> tt;
+
+  while (tt--) {
+    cin >> n;
+    cout << fibo(n) << "\n";
+  }
+
+  return 0;
+}
